c333f54909ab9fbe7bca31ee46d0771f63e9aa65
Jakub Hrabec <xhrabec@gmail.com>
2021-12-08 18:43:18 +0100

    day8

---
+++ 2021/8.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"log"
+	"strings"
+)
+
+type DisplayData struct {
+	digits []string
+	result []string
+}
+
+func parseDigits(line string) []string {
+	return strings.Split(line, " ")
+}
+
+func parseDisplayData(line string) DisplayData {
+	inAndOut := strings.Split(line, " | ")
+	return DisplayData{parseDigits(inAndOut[0]), parseDigits(inAndOut[1])}
+}
+
+func parseAllDisplayData(data []string) []DisplayData {
+	displays := make([]DisplayData, len(data))
+
+	for i, line := range data {
+		displays[i] = parseDisplayData(line)
+	}
+
+	return displays
+}
+
+func getNumUniqueDigits(displays []DisplayData) int {
+	total := 0
+
+	for _, display := range displays {
+		for _, digit := range display.result {
+			if len(digit) == 2 || len(digit) == 3 || len(digit) == 4 || len(digit) == 7 {
+				total += 1
+			}
+		}
+	}
+
+	return total
+}
+
+func toBits(digit string) int {
+	bits := 0
+
+	for _, c := range digit {
+		bits |= 1 << (c - 'a')
+	}
+
+	return bits
+}
+
+func decodeDisplayNumber(display DisplayData) int {
+	ds := make([]int, 10)
+	bds := []int{}
+
+	for _, digit := range display.digits {
+		bitDigit := toBits(digit)
+
+		if len(digit) == 2 {
+			ds[1] = bitDigit
+		} else if len(digit) == 3 {
+			ds[7] = bitDigit
+		} else if len(digit) == 4 {
+			ds[4] = bitDigit
+		} else if len(digit) == 7 {
+			ds[8] = bitDigit
+		} else {
+			bds = append(bds, bitDigit)
+		}
+	}
+
+	sA := ds[7] ^ ds[1]
+	sCF := ds[1]
+	sEG := ds[8] ^ (ds[4] | sA)
+	sBD := ds[4] ^ ds[1]
+
+	for _, bd := range bds {
+		if bd&(127^sEG) == (127^sEG) && bd^sEG != sEG {
+			ds[9] = bd
+		} else if bd&(127^sBD) == (127^sBD) && bd^sBD != sBD {
+			ds[0] = bd
+		} else if bd&(127^sCF) == (127^sCF) && bd^sCF != sCF {
+			ds[6] = bd
+		}
+	}
+
+	sE := 127 ^ ds[9]
+	sG := sEG ^ sE
+	sD := 127 ^ ds[0]
+	sB := sBD ^ sD
+	sC := 127 ^ ds[6]
+	sF := sCF ^ sC
+	ds[3] = ds[7] | sD | sG
+	ds[2] = 127 - sB - sF
+	ds[5] = ds[6] - sE
+
+	sum := 0
+	for _, digit := range display.result {
+		bitDigit := toBits(digit)
+		for n, b := range ds {
+			if b == bitDigit {
+				sum = sum*10 + n
+			}
+		}
+	}
+
+	return sum
+}
+
+func getDisplaysSum(displays []DisplayData) int {
+	sum := 0
+	for _, d := range displays {
+		sum += decodeDisplayNumber(d)
+	}
+	return sum
+}
+
+func main8() {
+	data, err := ReadInputFrom("8.inp")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	displays := parseAllDisplayData(data)
+
+	log.Println(getNumUniqueDigits(displays))
+	log.Println(getDisplaysSum(displays))
+}
