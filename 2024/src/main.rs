c4512cefffea4fe174c006d1e910422afeaf3af2
Jakub Hrabec <xhrabec@gmail.com>
2024-12-21 14:48:47 +0100

    remaining days skeleton

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -13,6 +13,16 @@ mod code_12;
 mod code_13;
 mod code_14;
 mod code_15;
+mod code_16;
+mod code_17;
+mod code_18;
+mod code_19;
+mod code_20;
+mod code_21;
+mod code_22;
+mod code_23;
+mod code_24;
+mod code_25;
 pub mod util;
 use regex::Regex;
 use std::env;
@@ -43,6 +53,16 @@ fn main() {
             "13" => code_13::run(&file_content),
             "14" => code_14::run(&file_content),
             "15" => code_15::run(&file_content),
+            "16" => code_16::run(&file_content),
+            "17" => code_17::run(&file_content),
+            "18" => code_18::run(&file_content),
+            "19" => code_19::run(&file_content),
+            "20" => code_20::run(&file_content),
+            "21" => code_21::run(&file_content),
+            "22" => code_22::run(&file_content),
+            "23" => code_23::run(&file_content),
+            "24" => code_24::run(&file_content),
+            "25" => code_25::run(&file_content),
             _ => panic!("Undefined code"),
         }
     }
